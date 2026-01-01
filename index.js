772db27956427e524f45c97475f488fa23c36c0c
Divyang9099 <gujaratidivyang212@gmail.com>
2026-01-01 12:14:28 +0530

    Force IPv4 for Gmail SMTP

--- index.js
+++ index.js
@@ -133,6 +133,10 @@ app.put('/api/orders/:id', async (req, res) => {
                         user: process.env.EMAIL_USER,
                         pass: process.env.EMAIL_PASS
                     },
+                    tls: {
+                        ciphers: 'SSLv3'
+                    },
+                    family: 4, // Force IPv4
                     debug: true,
                     logger: true
                 });

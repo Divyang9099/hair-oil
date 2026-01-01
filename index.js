78c15200ef3c73a4d7f0a93b82746f5e283451be
Divyang9099 <gujaratidivyang212@gmail.com>
2026-01-01 10:04:05 +0530

    Fix CORS policy to allow all origins

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+app.use(cors({
+    origin: '*', // Allow all origins to fix CORS issues
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
 
 // Order Schema
 const orderSchema = new mongoose.Schema({
